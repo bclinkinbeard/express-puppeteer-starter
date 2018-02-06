ec936419556556309a347a5a358d3adf21ab8a32
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2018-02-05 21:06:21 -0500

    Element screenshots

--- routes/images.js
+++ routes/images.js
@@ -32,10 +32,34 @@ async function getScreenshotFromURL(url, width = 800, height = 400) {
   return bufferPromise
 }
 
-router.get('/png', async function(req, res) {
+async function getScreenshotOfElementFromURL(
+  url,
+  selector,
+  width = 800,
+  height = 400,
+) {
+  const { browser, page } = await initPuppeteer()
+  page.setViewport({ width, height })
+  await goToURL(page, url)
+  const target = await page.waitForSelector(selector)
+  const bufferPromise = await target.screenshot()
+  await browser.close()
+  return bufferPromise
+}
+
+router.get('/page', async function(req, res) {
   const url = req.query.url || 'https://benclinkinbeard.com/d3in5days'
   const { w = 800, h = 400 } = req.query
   const imageBuffer = await getScreenshotFromURL(url, +w, +h)
+
+  await res.type('png').end(imageBuffer, 'binary')
+})
+
+router.get('/el', async function(req, res) {
+  const url = req.query.url || 'https://benclinkinbeard.com/d3in5days'
+  const selector = req.query.selector || '#export-container'
+  const { w = 800, h = 400 } = req.query
+  const imageBuffer = await getScreenshotOfElementFromURL(url, selector, +w, +h)
 
   await res.type('png').end(imageBuffer, 'binary')
 })
